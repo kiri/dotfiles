0093bbfde9d12552e211a29d5510580fd6a11923
kiri <kiri@users.noreply.github.com>
2024-10-22 19:59:57 +0900

    update

--- neovim/nvim/lua/plugins/oil.lua
+++ neovim/nvim/lua/plugins/oil.lua
@@ -5,7 +5,7 @@ return {
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-    lazy = true,
+    lazy = false,
     keys = { 
         {"-", "<CMD>Oil<CR>", { desc = "Open parent directory" }},
     },
